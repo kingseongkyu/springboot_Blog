51448170ebee911d0e26454e5ddeda450d50e4fb
kingseongkyu <seonggyuhan0@gmail.com>
2026-02-06 18:17:53 +0900

    2일차 finished

--- src/main/java/me/seongkyu/springbootdeveloper/Controller/BlogApiController.java
+++ src/main/java/me/seongkyu/springbootdeveloper/Controller/BlogApiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.seongkyu.springbootdeveloper.domain.Article;
 import me.seongkyu.springbootdeveloper.dto.AddArticleRequest;
 import me.seongkyu.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +16,14 @@ public class BlogApiController {
 
     private final BlogService blogService;
 
-    @PostMapping("/api/articles")
+    @PostMapping("/api/articles")   //HTTP메서드가 POST일 때 요청받은 URL과 동일한 메서드와 매핑
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        //HTTP 요청할 때 응답에 해당하는 값을 @RequestBody애너붙은 객체인 AddAriticleRequest에 매핑
+
         Article savedArticle = blogService.save(request);
-
-
+    //요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED)    //응답코드 201, Created를 응답하고 테이블에 저장된 객체 반환
+            .body(savedArticle);
     }
 
 }
